5b69d59e222338b4836fe0b2ffd016e40d383105
duyduc130602 <nguyenduc130602@gmail.com>
2022-10-30 23:27:15 +0700

    crud chip and create template

---
+++ src/app/_model/product.ts
@@ -0,0 +1,11 @@
+export class Product {
+  id!: number;
+  name!: string;
+  price!: number;
+  quantity!: number;
+  discount!: number;
+  description!: string;
+  status!: number;
+  categoryId!: number;
+  voucherId!: number;
+}
